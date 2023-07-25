bbea20b8cfa1014b6b984aff48d75c7902343f5a
toybreaker <toybreaker@users.noreply.github.com>
2023-07-25 13:54:36 +0800

    starting | v0.0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,32 @@
 import { defineConfig } from 'astro/config';
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    assets: true
+  },
+  output: 'static',
+  build: {
+    // The 'auto' option will inline only the stylesheets that are below the vite.build.assetsInlineLimit threshold,
+    // reducing the number of requests for smaller sheets.
+    // Larger stylesheets, such as global ones used by all pages,
+    // will still be fetched externally and cached:
+    // inlineStylesheets: 'auto'
+
+    // inline all stylesheets:
+    inlineStylesheets: 'always'
+  },
+  integrations: [partytown({
+    // Example: Add dataLayer.push as a forwarding-event.
+    config: {
+      forward: ['dataLayer.push']
+    }
+  }),
+    sitemap(), robotsTxt()],
+  outDir: './dist',
+  site: 'https://roccomarosi.com/',
+  base: '/'
+});
