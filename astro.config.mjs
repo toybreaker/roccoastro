0376db94413ab32beb43a32d2d5ca5682a7883f6
toybreaker <toybreaker@users.noreply.github.com>
2023-07-30 18:11:14 +0800

    partytown wtf | v0.1.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
-import partytown from '@astrojs/partytown'
 import sitemap from '@astrojs/sitemap'
 import robotsTxt from 'astro-robots-txt'
+import partytown from '@astrojs/partytown'
 import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
@@ -9,25 +9,15 @@ export default defineConfig({
   experimental: {
     assets: true
   },
-  output: 'static',
-  build: {
-    // The 'auto' option will inline only the stylesheets that are below the vite.build.assetsInlineLimit threshold,
-    // reducing the number of requests for smaller sheets.
-    // Larger stylesheets, such as global ones used by all pages,
-    // will still be fetched externally and cached:
-    // inlineStylesheets: 'auto'
-
-    // inline all stylesheets:
-    inlineStylesheets: 'always'
+  markdown: {
+    drafts: true
   },
   integrations: [
-    sitemap(),
-    robotsTxt(),
     mdx({
-      extendMarkdownConfig: false,
-      gfm: true,
       drafts: true
     }),
+    sitemap(),
+    robotsTxt(),
     partytown({
       // Example: Add dataLayer.push as a forwarding-event.
       config: {
@@ -37,5 +27,15 @@ export default defineConfig({
   ],
   outDir: './dist',
   site: 'https://n.roccomarosi.com/',
-  base: '/'
+  base: '/',
+  build: {
+    // The 'auto' option will inline only the stylesheets that are below the vite.build.assetsInlineLimit threshold,
+    // reducing the number of requests for smaller sheets.
+    // Larger stylesheets, such as global ones used by all pages,
+    // will still be fetched externally and cached:
+    // inlineStylesheets: 'auto'
+
+    // inline all stylesheets:
+    inlineStylesheets: 'always'
+  }
 })
